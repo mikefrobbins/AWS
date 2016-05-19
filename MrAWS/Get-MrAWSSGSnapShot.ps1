a8af63aa1bfc3c634f113eb409ce942f815f1f73
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-05-19 14:14:57 -0500

    Moved required modules from manifest to individual scripts

--- MrAWS/Get-MrAWSSGSnapShot.ps1
+++ MrAWS/Get-MrAWSSGSnapShot.ps1
@@ -1,5 +1,4 @@
-﻿#Requires -Version 3.0
-#Add 'Requires -Modules AWSPowerShell' if used without the script module
+﻿#Requires -Version 3.0 -Modules AWSPowerShell
 function Get-MrAWSSGSnapShot {
 
 <#
